dd07bcdfc4ed1206de72bf01d55ce7660057427a
Filip Mossberg <filip.mossberg@gmail.com>
2022-11-07 13:01:40 +0100

    Add project files.

---
+++ Labb4-CollectionGenerics/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb4_CollectionGenerics
+{
+    internal class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+        public Employee(int _Id, string _Name, string _Gender, double _Salary)
+        {
+            this.Id = _Id;
+            this.Name = _Name;
+            this.Gender = _Gender;
+            this.Salary = _Salary;
+        }
+        public override string ToString()
+        {
+            return $"ID = {Id}, Name = {Name}, Gender = {Gender}, Salary = {Salary}";
+        }
+    }
+}
---
+++ Labb4-CollectionGenerics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb4_CollectionGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1001, "Filip", "M", 20000);
+            Employee employee1 = new Employee(1002, "Alvin", "M", 25000);
+            Employee employee2 = new Employee(1003, "Isabella", "F", 20000);
+            Employee employee3 = new Employee(1003, "Joakim", "M", 30000);
+            Employee employee4 = new Employee(1003, "Ulrika", "F", 35000);
+
+            Stack<Employee> EmpList = new Stack<Employee>();
+            EmpList.Push(employee);
+            EmpList.Push(employee1);
+            EmpList.Push(employee2);
+            EmpList.Push(employee3);
+            EmpList.Push(employee4);
+
+            Console.WriteLine("List of all users:");
+            foreach (Employee emp in EmpList)
+            {
+                Console.WriteLine(emp);
+                //Console.WriteLine($"ID = {emp.Id}, Name = {emp.Name}, Gender = {emp.Gender}, Salary = {emp.Salary}");
+                Console.WriteLine($"Items in lsit: {EmpList.Count}");
+
+            }
+
+        }
+    }
+}
